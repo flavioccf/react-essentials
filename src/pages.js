19677e19f3ba6e754cb884c478f3a31750a39591
flavioccf <flavioccf@hotmail.com>
2021-03-03 00:06:30 -0300

    remove unused data

--- src/pages.js
+++ src/pages.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, useLocation,   useParams,
-    useRouteMatch ,Switch, Route} from "react-router-dom";
+import { Link, useLocation, useRouteMatch ,Switch, Route} from "react-router-dom";
 
 function LinkNav() {
     return (    <nav>
